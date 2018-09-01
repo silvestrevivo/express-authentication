da86f15996118da534b840fddaf78c1858e387e4
silvestrevivo <silvestrevivomillan@gmail.com>
2018-09-02 00:01:51 +0200

    creating tokens with jwt-simple

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,15 @@
 'use strict'
+const jwt = require('jwt-simple')
 const User = require('../models/user')
+const config = require('../config')
+
+function tokenForUser(user) {
+  // we use id because that doesn't change ever
+  // sub is a convention => tokens have a sub object
+  const timeStamp = new Date().getTime()
+  return jwt.encode({ sub: user.id, iat: timeStamp }, config.secret)
+  // the first parameters define the token, and those values has to be unique
+}
 
 const singup = (req, res, next) => {
   const { email, password } = req.body
@@ -27,7 +37,7 @@ const singup = (req, res, next) => {
     user.save(err => {
       if (err) return next(err)
       // Respond to request indicating the user was created
-      res.json(user)
+      res.json({ token: tokenForUser(user) })
     })
   })
 }
