f51f37b720190411a3314d6732476a19c10fe06d
silvestrevivo <silvestrevivomillan@gmail.com>
2018-09-06 21:47:26 +0200

    server folder created to recive client

---
+++ server/index.js
@@ -0,0 +1,29 @@
+'use strict'
+// Main starting point of the application
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+const router = require('./routes/router')
+const mongoose = require('mongoose')
+
+// DB Setup
+mongoose.connect(
+  'mongodb://localhost:27017/auth',
+  { useNewUrlParser: true },
+)
+mongoose.set('useCreateIndex', true)
+
+// App setup
+app.use(morgan('combined')) //logging framework for debugging
+app.use(bodyParser.json({ type: '*/*' })) // parse incomming requests
+// { type: '*/*' } => parse on any request
+router(app)
+
+// Server setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+
+console.log(`server listening on: ${port}`)
